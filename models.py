844bb235e37fdfb5541f0c97804ee77555d48083
Ulysses Uribe <ulysses.uribe@gmail.com>
2021-05-12 11:22:50 -0700

    fixed table reference

--- models.py
+++ models.py
@@ -21,11 +21,22 @@ def get_username():
 
 
 db.define_table(
+    'address',
+    # TODO_complete: define the fields that are in the json.
+    Field('reviews_landlord_id', 'reference auth_user'),
+    Field('address_property_address', requires=IS_NOT_EMPTY(error_message=T('Property Address Required'))),
+)
+
+db.address.id.readable = db.address.id.writable = False
+db.address.reviews_landlord_id.readable = db.address.reviews_landlord_id.writable = False
+
+
+db.define_table(
     'reviews',
     # TODO_complete: define the fields that are in the json.
     Field('reviews_renters_id', 'reference auth_user'),
     Field('reviews_landlord_id', 'reference auth_user'),
-    # Field('reviews_address_id', 'reference address'),
+    Field('reviews_address_id', 'reference address'),
     Field('reviews_contents', requires=IS_NOT_EMPTY(error_message=T('Review cannot be empty'))),
     Field('reviews_score_overall', default='0'),
     Field('reviews_score_responsiveness', default='0'),
@@ -35,21 +46,23 @@ db.define_table(
     Field('thumbs_down', defualt='0'),
 )
 
+db.reviews.id.readable = db.reviews.id.writable = False
 db.reviews.reviews_renters_id.readable = db.reviews.reviews_renters_id.writable = False
 db.reviews.reviews_landlord_id.readable = db.reviews.reviews_landlord_id.writable = False
-# db.reviews.reviews_address_id.readable = db.reviews.reviews_address_id.writable = False
+db.reviews.reviews_address_id.readable = db.reviews.reviews_address_id.writable = False
 db.reviews.thumbs_up.readable = db.reviews.thumbs_up.writable = False
 db.reviews.thumbs_down.readable = db.reviews.thumbs_down.writable = False
 
 
-db.define_table(
-    'address',
-    # TODO_complete: define the fields that are in the json.
-    Field('reviews_landlord_id', 'reference auth_user'),
-    Field('address_property_address', requires=IS_NOT_EMPTY(error_message=T('Property Address Required'))),
-)
-
-db.address.reviews_landlord_id.readable = db.address.reviews_landlord_id.writable = False
+# db.define_table(
+#     'address',
+#     # TODO_complete: define the fields that are in the json.
+#     Field('reviews_landlord_id', 'reference auth_user'),
+#     Field('address_property_address', requires=IS_NOT_EMPTY(error_message=T('Property Address Required'))),
+# )
+#
+# db.address.id.readable = db.address.id.writable = False
+# db.address.reviews_landlord_id.readable = db.address.reviews_landlord_id.writable = False
 
 db.define_table(
     'review_replies',
